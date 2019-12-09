19ab82d53c1cc3af024209d8e4df4b03e251b281
Jonathan Gonzalez <jgonzalez249@knights.ucf.edu>
2019-12-08 19:44:51 -0500

    Final Project
    This is still a work in progress, very rough

---
+++ Final Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject [] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    //Text for UI
+    public Text scoreText;
+    public Text restartText;
+    public Text winText;
+    public Text gameOverText;
+    public Text timeAttackText;
+    public Text timerText;
+
+    //Timer for Time Attack
+    private float timer;
+
+    //Boolean States
+    private bool timeAttack;
+    private bool gameOver;
+    private bool restart;
+
+    //Score for game
+    private int score;
+
+    //Win and Lose Music
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
+
+    private PlayerController playerController;
+
+    void Start()
+    {
+        GameObject playerControllerObject = GameObject.FindWithTag("Player");
+        if (playerControllerObject != null)
+        {
+            playerController = playerControllerObject.GetComponent<PlayerController>();
+        }
+        if (playerController == null)
+        {
+            Debug.Log("Cannot find 'PlayerController' script");
+        }
+
+
+
+        timeAttack = false;
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        winText.text = "";
+        gameOverText.text = "";
+        timeAttackText.text = "";
+        timerText.text = "";
+
+        timer = 30.0f;
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                SceneManager.LoadScene("Main");
+            }
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                SceneManager.LoadScene("Main");
+                timeAttack = true;
+            }
+
+           
+
+            if (timeAttack)
+            {
+                spawnWait = 0.2f;
+                timer -= Time.deltaTime;
+                timerText.text = "Time Left: " + timer;
+                     
+                 if(timer < 0)
+                 {
+                   restart = true;
+                 }
+               
+
+            }
+        }
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+
+            if (gameOver)
+            {
+             
+                restartText.text = "Press 'G' for Restart";
+                timeAttackText.text = "Press 'B' for Time Attack";
+                restart = true;
+                timeAttack = true;
+                break;
+            }
+        }
+    }
+
+   
+
+
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You win!" +
+                "\n"+
+                "Game Created by Jonathan Gonzalez";
+            gameOver = true;
+            restart = true;
+            timeAttack = true;
+
+
+            Destroy(playerController.GetComponent<MeshCollider>());
+            
+            //Might use this for final project
+            //Destroy(playerController);            
+                    
+        }
+
+        //Stop BG music and switch to Game Over Music
+        if (gameOver)
+        {
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            musicSource.loop = true;
+
+
+            if (score >= 100)
+            {
+                musicSource.Stop();
+                musicSource.PlayOneShot(musicClipOne, 0.7F);
+                musicSource.loop = true;
+
+            }
+        }
+    }
+
+   public void GameOver()
+    {
+        gameOverText.text = "Game Over!" + 
+            "\n" +
+            "Game Created by Jonathan Gonzalez";
+        gameOver = true;
+    }
+
+    public void AttackOver()
+    {
+        timeAttack = true;
+    }
+}
---
+++ Final Project/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public PlayerController playerController;
+
+    public GameObject poweredShot;
+    public float duration;
+
+    public Transform shotSpawn;
+    private float nextFire;
+    private AudioSource sfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playerControllerObject = GameObject.FindWithTag("Player");
+        if (playerControllerObject != null)
+        {
+            playerController = playerControllerObject.GetComponent<PlayerController>();
+        }
+        if (playerController == null)
+        {
+            Debug.Log("Cannot find 'PlayerController' script");
+        }
+
+        sfx = playerController.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   /* void PoweredUp()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            sfx.Play();
+        }
+    }*/
+}
